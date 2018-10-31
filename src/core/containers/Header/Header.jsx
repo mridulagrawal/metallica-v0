a67c5e5f28332cc90e993c6ca09956402a612e1c
Mridul Agrawal <m3121995@gmail.com>
2018-10-31 21:52:57 +0530

    folder structure v0

---
+++ src/core/containers/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import HeaderComponent from '../../components/Header/Header.component';
+
+class Header extends Component {
+  render() {
+    return (
+        <HeaderComponent/>
+    );
+  }
+}
+
+export default Header;
